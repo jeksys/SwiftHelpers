5fc92f1b224aca814830f38e6386cb2a39075fac
Eugene Yagrushkin <eugene@castingworkbook.com>
2017-05-23 18:43:08 -0700

    no message

--- Sources/IBDesignableHelpers.swift
+++ Sources/IBDesignableHelpers.swift
@@ -63,3 +63,36 @@
     }
     
 }
+
+@IBDesignable extension UIButton {
+
+    @IBInspectable var kern: CGFloat{
+        get {
+            return 0
+        }
+        set{
+            self.kern(kerningValue: newValue)
+        }
+    }
+    
+    func kern(kerningValue:CGFloat) {
+        let attributedText =  NSAttributedString(string: self.titleLabel!.text!, attributes: [NSKernAttributeName:kerningValue, NSFontAttributeName:self.titleLabel!.font, NSForegroundColorAttributeName:self.titleLabel!.textColor])
+        self.setAttributedTitle(attributedText, for: .normal)
+    }
+}
+
+@IBDesignable extension UILabel {
+
+    @IBInspectable var kern: CGFloat{
+        get {
+            return 0
+        }
+        set{
+            self.kern(kerningValue: newValue)
+        }
+    }
+
+    func kern(kerningValue:CGFloat) {
+        self.attributedText =  NSAttributedString(string: self.text ?? "", attributes: [NSKernAttributeName:kerningValue, NSFontAttributeName:font, NSForegroundColorAttributeName:self.textColor])
+    }
+}
