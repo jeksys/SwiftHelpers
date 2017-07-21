da72e0c6c7a2fea465b2b966a7b8cf1bf3303c1d
Eugene Yagrushkin <yagrushkin@gmail.com>
2017-07-20 17:34:52 -0700

    added universal json date formatter

--- Sources/ASPNETDateMapper.swift
+++ Sources/ASPNETDateMapper.swift
@@ -45,25 +45,39 @@ open class ASPNETDateTransformShort2: DateFormatterTransform {
     
 }
 
-open class  ASPNETDateTransformTest: TransformType {
+open class  ASPNETDateTransformAll: TransformType {
     public typealias Object = Date
     public typealias JSON = String
     
-    public let dateFormatter: DateFormatter
-    
-    public init(dateFormatter: DateFormatter) {
-        self.dateFormatter = dateFormatter
+    public init() {
     }
     
     open func transformFromJSON(_ value: Any?) -> Date? {
         if let dateString = value as? String {
-            return dateFormatter.date(from: dateString)
+            let formatter = DateFormatter()
+            formatter.locale = Locale(identifier: "en_US_POSIX")
+
+            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZ"
+            if let date = formatter.date(from: dateString){
+                return date
+            }
+            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
+            if let date = formatter.date(from: dateString){
+                return date
+            }
+            formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+            if let date = formatter.date(from: dateString){
+                return date
+            }
         }
         return nil
     }
     
     open func transformToJSON(_ value: Date?) -> String? {
         if let date = value {
+            let dateFormatter = DateFormatter()
+            dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZ"
             return dateFormatter.string(from: date)
         }
         return nil
