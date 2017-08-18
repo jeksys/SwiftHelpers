73f1ebe302ff34c0fb4219ca00277e9cf10955de
Eugene Yagrushkin <yagrushkin@gmail.com>
2017-08-18 12:11:33 -0700

    no message

--- Sources/StringHelpers.swift
+++ Sources/StringHelpers.swift
@@ -39,3 +39,20 @@ extension String {
         return substring(with: startIndex..<endIndex)
     }
 }
+
+extension String {
+    
+    func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSFontAttributeName: font], context: nil)
+        
+        return ceil(boundingBox.height)
+    }
+    
+    func width(withConstraintedHeight height: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSFontAttributeName: font], context: nil)
+        
+        return ceil(boundingBox.width)
+    }
+}
