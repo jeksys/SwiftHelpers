1eb56ee92203d1a02335e37f10acd797dff0acb9
Eugene Yagrushkin <yagrushkin@gmail.com>
2017-08-21 15:36:10 -0700

    added md5 for a string

---
+++ Sources/String+MD5.swift
@@ -0,0 +1,37 @@
+//
+//  String+MD5.swift
+//  SelfTaping
+//
+//  Created by Evgeny Yagrushkin on 2017-08-21.
+//  Copyright © 2017 Casting Workbook Services Inc. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    var md5: String? {
+        guard let data = self.data(using: String.Encoding.utf8) else { return nil }
+        
+        let hash = data.withUnsafeBytes { (bytes: UnsafePointer<Data>) -> [UInt8] in
+            var hash: [UInt8] = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
+            CC_MD5(bytes, CC_LONG(data.count), &hash)
+            return hash
+        }
+        
+        return hash.map { String(format: "%02x", $0) }.joined()
+    }
+    
+    func sha1() -> String {
+        let data = self.data(using: String.Encoding.utf8)!
+        var digest = [UInt8](repeating: 0, count:Int(CC_SHA1_DIGEST_LENGTH))
+        data.withUnsafeBytes {
+            _ = CC_SHA1($0, CC_LONG(data.count), &digest)
+        }
+        let hexBytes = digest.map { String(format: "%02hhx", $0) }
+        return hexBytes.joined()
+    }
+    
+    
+    
+}
