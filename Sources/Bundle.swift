b78cce2245dae4a3577997dbc374bc09c5d723f4
Eugene Yagrushkin <yagrushkin@gmail.com>
2017-09-29 16:56:33 -0700

    added Bundle extension

---
+++ Sources/Bundle.swift
@@ -0,0 +1,22 @@
+//
+//  Bundle.swift
+//  SelfTaping
+//
+//  Created by Evgeny Yagrushkin on 2017-09-29.
+//  Copyright © 2017 Casting Workbook Services Inc. All rights reserved.
+//
+
+extension Bundle{
+    func readJson(fileName: String) -> String? {
+        do {
+            if let file = self.url(forResource: fileName, withExtension: "json") {
+                let data = try Data(contentsOf: file)
+                let jsonString = String(data: data, encoding: .utf8)
+                return jsonString
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        return nil
+    }
+}
