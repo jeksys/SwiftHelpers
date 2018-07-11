99507b7bd6857abbf3c9fde833c373f125cc3306
Evgeny Yagrushkin <yagrushkin@gmail.com>
2018-07-10 19:19:35 -0700

    bump

--- Sources/StringHelpers.swift
+++ Sources/StringHelpers.swift
@@ -19,14 +19,31 @@ extension String {
     // show erros if localization is missing
     var localized: String {
         //TODO: record all unresolved keys
-        let localizedString = NSLocalizedString(self, comment: "")
-        if localizedString == self{
-            // no localization or localization is not keyed
+        var localizedString = NSLocalizedString(self, comment: "")
+        if localizedString == self {
+            localizedString = NSLocalizedStringFallback(self, comment: "")
+            if LogOptions.localization {
+                if localizedString == self {
+                    print("missing localization: \(self)")
+                }else{
+                    print("missing localization for \(String(describing: Locale.current.languageCode)): \(self)")
+                }
+            }
         }
         LocalizationManager.shared.addLocalization(key: self, string: localizedString)
         return localizedString
     }
+    
+    public func NSLocalizedStringFallback(_ key: String, tableName: String? = nil, bundle: Bundle = Bundle.main, value: String = "", comment: String) -> String {
+        let fallbackLanguage = "Base"
+        guard let fallbackBundlePath = Bundle.main.path(forResource: fallbackLanguage, ofType: "lproj") else { return key }
+        guard let fallbackBundle = Bundle(path: fallbackBundlePath) else { return key }
+        let fallbackString = fallbackBundle.localizedString(forKey: key, value: comment, table: nil)
+        return Bundle.main.localizedString(forKey: key, value: fallbackString, table: nil)
+    }
 }
+
+
 
 // indexes
 extension String {
